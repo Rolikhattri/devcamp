697c1e0f9f0c92b963cfdf710b4405e1c5907323
rolitandon <roli_tandon@bitbucket.org>
2018-07-07 19:13:41 +0530

    new create

--- app/controllers/portfolios_controller.rb
+++ app/controllers/portfolios_controller.rb
@@ -1,2 +1,34 @@
 class PortfoliosController < ApplicationController
+
+def index
+  @portfolio_items = Portfolio.all
+end
+
+
+def new
+  @portfolio_item = Portfolio.new
+end
+
+def create
+  @portfolio_item = Portfolio.new(portfolio_params)
+
+
+    respond_to do |format|
+      if @portfolio_item.save
+        format.html { redirect_to portfolios_path, notice: 'Portfolio was successfully created.' }
+        format.json { render :show, status: :created, location: @portfolio }
+      else
+        format.html { render :new }
+        format.json { render json: @portfolio.errors, status: :unprocessable_entity }
+      end
+    end
+
+end
+
+
+private
+ # Never trust parameters from the scary internet, only allow the white list through.
+    def portfolio_params
+      params.require(:portfolio).permit(:title,:subtitle, :body)
+    end
 end
