ce6e205f2435106a5aa22abd53ccd971ac12633e
rolitandon <roli_tandon@bitbucket.org>
2018-07-07 20:43:04 +0530

    show

--- app/controllers/portfolios_controller.rb
+++ app/controllers/portfolios_controller.rb
@@ -4,6 +4,10 @@ def index
   @portfolio_items = Portfolio.all
 end
 
+def show
+     @portfolio_item = Portfolio.find(params[:id])
+end
+
 
 def new
   @portfolio_item = Portfolio.new
@@ -24,7 +28,24 @@ def create
     end
 
 end
+def edit
+     @portfolio_item = Portfolio.find(params[:id])
 
+end
+
+def update
+       @portfolio_item = Portfolio.find(params[:id])
+
+    respond_to do |format|
+      if @portfolio_item.update(portfolio_params)
+        format.html { redirect_to portfolios_path, notice: 'Portfolio was successfully updated.' }
+        format.json { render :show, status: :ok, location: @portfolio }
+      else
+        format.html { render :edit }
+        format.json { render json: @portfolio.errors, status: :unprocessable_entity }
+      end
+    end
+  end
 
 private
  # Never trust parameters from the scary internet, only allow the white list through.
